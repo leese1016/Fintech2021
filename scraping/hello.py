0db9556c5a66a2504ac100ca44d42aced29f83c1
Seung eun Lee <>
2021-03-01 22:00:24 +0900

    기사 내용 타이틀 스크래핑하기

--- scraping/hello.py
+++ scraping/hello.py
@@ -2,4 +2,8 @@ from selenium import webdriver
 driver = webdriver.Chrome('/Users/seungeunlee/Desktop/Fintech2021/scraping/chromedriver')
 driver.implicitly_wait(3)
 driver.get(
-    'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=100&oid=005&aid=0001333549')
+    'https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=105&oid=138&aid=0002099390')
+title = driver.find_element_by_id('articleTitle')
+body = driver.find_element_by_id('articleBodyContents')
+print(title.text)
+print(body.text)
